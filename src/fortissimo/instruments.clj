a40553df56e152bd023366ad4867d8ad524add5e
jtcramer <jason.t.cramer@gmail.com>
2013-05-12 08:00:46 -0700

    added basic instrument importing on the clojure end

--- src/fortissimo/instruments.clj
+++ src/fortissimo/instruments.clj
@@ -7,6 +7,15 @@
 (defn getDuration [duration tempo]
   (/ (* duration 60.0) tempo))
 
+
+;; For user defined instruments, must accept keyword parameters, the very least being freq and sustain.
+(defn importInstrument [filename]
+  (load-file filename))
+
+; User defined instrument should be wrapped in this function
+(defn userInstrument [name_, instr]
+  (defn instrs (assoc instrs name_ instr)))
+
 (definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
     (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
        (saw freq)
