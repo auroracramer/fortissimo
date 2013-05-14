b0e3c34491d927c4a228f3dbe2cc5e510ad4966f
jtcramer <jason.t.cramer@gmail.com>
2013-05-14 07:07:58 -0700

    got packaging stuff to work, can now execute as standalone jar suit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject fortissimo "1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.8.1"]]
+  :source-paths ["src/" "src/fortissimo" "target/native/macosx/x86_84/"]
+  :main fortissimo.fortissimo_main)
